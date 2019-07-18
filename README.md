173f694922df987c06c41261c153cf20f3411014
Lei Shi <Lei_Shi@comcast.com>
2019-07-18 14:10:29 -0400

    finish main part of vector

--- README.md
+++ README.md
@@ -3,10 +3,16 @@
 C++ is great and powerful, but lacks some very basic 
 functions like `string.split`, `vector.group_by`, etc.
 This library allows you to write them
-in C++ like in Ruby, which has sweeeeeet syntactic sugar.
+in C++ like in Ruby. Syntactic sugar is sweeeet!
+
 It's basically a wrapper for `std::string` and some
 containers, extending them with the functionality of
-their counterpart in Ruby.
+their counterpart in Ruby. 
+
+I'm conservative and trying
+to stay compatible with native C++. For example, elements
+are not assumed to always be pointers, and there's no 
+universal parent type "Object".
 
 ## Dependency
 A compiler that supports C++11
@@ -26,19 +32,26 @@ Hash
 Enumerator  
 ......
 
-Anywhere in Ruby a block is required, use a C++ lambda.
+C++ lambda is equivalence of block in Ruby.
 
-If the std container already has a member function, 
-(e.g., `push_back`, `pop_back`, `size`),
-it won't be overridden. Otherwise, the function name and 
-behavior will be the same as in Ruby. `!` character is
-not allowed in C++ as identifier, so it's replaced by
-`_` (underscore). `?` is not allowed either. But it incurs
-no ambiguity. So it's simply removed.
+If the std container already has a function, 
+(e.g., `push_back`), it won't be overridden(e.g. by `append`). 
+Otherwise, the function name and 
+behavior will be the same as in Ruby(if possible). 
+`!` character is not allowed in C++ as identifier, 
+so it's replaced by `_` (underscore). `?` is not allowed either. 
+But it incurs no ambiguity. So it's simply removed.
 
 Container elements are copied by value, unlike in Ruby,
 where everything is an object. If you want things to
 work the Ruby way, pass around pointers for everything.
+
+Types can't be mixed up in a container, nor can elements' 
+type be changed(e.g. by `map_`), unless you define everything 
+as subtype of an "Object" class like in Ruby.
+
+To break or continue within a lambda, use `break_` and
+`continue_`. Currently only supported by `each`.
 
 A macro `S_` (capital S then underscore) is defined for
 string interpolation. `S_( exp )` will evaluate `exp`
@@ -53,8 +66,20 @@ Output:
 `1 plus 2 equals 3`
 
 I'm also defining a macro `puts`, which is the same as
-the Ruby `puts`. If you find any of the macros conflicting
-with your stuff, undefine it.
+the Ruby `puts`. If you find it conflicting with your stuff, 
+undefine it.
+
+Some tips on using C++ if you've never used C++11 or later:
+
+A lambda is essentially a function pointer, but can
+optionally derive local context.
+The most brainless way to define a lambda is like this:
+`
+[&]( $arguments ) -> $return_type {
+	$body
+}
+`
+It derives all local contexts and should work in most cases.
 
 Try to use `auto` feature in C++. It saves you a lot of 
 effort typing the type name.
--- rubify.hpp
+++ rubify.hpp
@@ -10,94 +10,199 @@
 
 namespace Rubify {
 
-#define puts(exp) std::cout<<(exp)<<std::endl
+/* ======================== Definition ===========================*/
 
-#define S_(exp) +( [&]()->std::string { \
-	std::stringstream ss;\
-	ss << (exp);\
-	return ss.str();\
-}() )+
+	#define puts(exp) std::cout<<(exp)<<std::endl
 
-	class string;
+	#define S_(exp) +( [&]()->std::string { \
+		std::stringstream ss;\
+		ss << (exp);\
+		return ss.str();\
+	}() )+
+
+	enum RubifyExecptionType { ERROR, NEXT, BREAK };
+
+	class string: public std::string {
+		public:
+		string( std::string src) { 
+			this->std::string::operator =(src);
+		}
+
+		bool operator <(string rival) {
+			return compare(rival) > 0;
+		}
+
+		/* implementation moved below to break dependency cycle */
+		vector<string> split(std::string delimiter);
+	};
+
+	class RubifyExecption {
+		public:
+		RubifyExecption(string msg) {
+			type = ERROR;
+			this->msg = msg
+		}
+
+		RubifyExecption(RubifyExecptionType type, string msg) {
+			this->type = type;
+			this->msg = msg;
+		}
+
+		RubifyExecptionType type;
+		string msg;
+	}
+
+	void continue_ () {
+		throw RubifyExecption(NEXT, "Next");
+	}
+
+	void break_ () {
+		throw RubifyExecption(BREAK, "Next");
+	}
 
 	template<typename T>
 	class vector: public std::vector<T> {
 		public:
 
-		vector<T>& each( void lambda(T&) );
+		vector( std::vector<T> src) { 
+			this->std::vector<T>::operator =(src);
+		}
 
-		template<typename T2>
-		vector< vector<T> > group_by( T2 lambda(T&) );
+		T& operator[](int index) {
+			if (index >= 0)
+				return this->std::vector<T>::operator [](index);
+			else
+				return this->std::vector<T>::operator [](size() + index);
+		}
 
-		template<typename T2>
-		vector<T> sort_by( T2 lambda(T& element) ) {
+		vector<T> take(size_t count) {
+			vector<T> ret;
+			for (int i=0; i<count; i++) {
+				ret.push_back( (*this)[i] );
+			}
+
+
+		template<typename T>
+		vector<T>& each( void lambda(T& element) ) {
+			for (int i=0; i<size(); i++)
+			{
+				try 
+				{
+					lambda((*this)[i]);
+				}
+				catch (RubifyExecption e)
+				{
+					if (e.type == ERROR)
+						throw e;
+					else if (e.type == BREAK)
+						break;
+					else
+						continue;
+				}
+			}
+			return (*this);
 		}
 
 		template<typename T2>
-		vector<T> sort_by_( T2 lambda(T& element) ) {
+		vector< vector<T> > group_by( T2 lambda(T element) ) {
+			vector< vector<T> > ret;
+			std::map< T2, vector<T> > boxes;
+			for (int i=0; i<size(); i++)
+			{
+				T value = (*this)[i];
+				T2 key = lambda(value);
+				boxes[key].push_back(value);
+			}
+			for_each(boxes.begin(), boxes.end(), [&](std::pair< T2, vector<T> > p) {
+				ret.push_back(p.second);
+			});
+			return ret;
+		}
+
+		/* '<' operator must be defined for T2 */
+		template<typename T2>
+		vector<T> sort_by( T2 lambda(T element) ) {
+			vector<T> cp = *this;
+			cp.sort_by_(lambda);
+			return cp;
 		}
 
 		template<typename T2>
-		vector<T2> map( T2 lambda(T& element) ) {
+		vector<T>& sort_by_( T2 lambda(T element) ) {
+			std::sort(begin(), end(), [&](T a, T b) -> bool {
+				return lambda(a) < lambda(b);
+			}
+			return *this;
 		}
 
 		template<typename T2>
-		vector<T2> map_( T2 lambda(T& element) ) {
+		vector<T2> map( T2 lambda(T element) ) {
+			vector<T2> ret;
+			each( [&](T& element){
+				ret.push_back(lambda(element));
+			})
+			return ret;
 		}
+
+		vector<T>& map_( T lambda(T& element) ) {
+			each( [&](T& element){
+				element = lambda(element);
+			})
+			return *this;
+		}
+
+	
+		/* If lengths are different, the function will 
+			only zip until the end of the shorter one.
+			The rest are DROPPED */
+		vector< vector<T> > zip( vector<T> buddy ) {
+			size_t len = size() < buddy.size() ? size() : buddy.size();
+			vector< vector<T> > ret;
+			for (int i=0; i<len; i++)
+			{
+				vector<T> zipped;
+				zipped.push_back( (*this)[i] );
+				zipped.push_back( buddy[i] );
+				ret.push_back(zipped);
+			}
+			return ret;
+		}
+
 
 	};
+
+	template<T>
+	class vector< vector<T> >: public std::vector< vector<T> > {
+		vector<T> flatten() {
+			vector<T> ret;
+			each( [&](T& sub_vector) {
+				sub_vector.each( [&](T& element) {
+					ret.push_back(element);
+				})
+			})
+			return ret;
+		}
+	}
 
 	template<>
 	class vector<string>: public std::vector<string> {
 		public:
-		string join(std::string delimiter);
+		string join(std::string delimiter) {
+			if (size() == 0)
+				return std::string("");
+			string ret = (*this)[0];
+			for (int i=1; i<size(); i++)
+			{
+				ret += delimiter;
+				ret += (*this)[i];
+			}
+			return ret;
+		}
 	};
 
-	class string: public std::string {
-		public:
-		string( std::string src);
-		vector<string> split(std::string delimiter);
-	};
 
-	template<typename T>
-	vector<T>& vector<T>::each( void lambda(T&) ) {
-		for (int i=0; i<this->size(); i++)
-			lambda((*this)[i]);
-		return (*this);
-	}
+/* ======================== Implementation ===========================*/
 
-	template<typename T>
-	template<typename T2>
-	vector< vector<T> > vector<T>::group_by( T2 lambda(T&) ) {
-		vector< vector<T> > ret;
-		std::map< T2, vector<T> > boxes;
-		for (int i=0; i<this->size(); i++)
-		{
-			T value = (*this)[i];
-			T2 key = lambda(value);
-			boxes[key].push_back(value);
-		}
-		for_each(boxes.begin(), boxes.end(), [&](std::pair< T2, vector<T> > p) {
-			ret.push_back(p.second);
-		});
-		return ret;
-	}
-
-	string::string( std::string src) { 
-		this->std::string::operator =(src);
-	}
-
-	string vector<string>::join(std::string delimiter) {
-		if (this->size() == 0)
-			return std::string("");
-		string ret = (*this)[0];
-		for (int i=1; i<this->size(); i++)
-		{
-			ret += delimiter;
-			ret += (*this)[i];
-		}
-		return ret;
-	}
 
 	vector<string> string::split(std::string delimiter) {
 		string remain = *this;
